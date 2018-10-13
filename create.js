8c83eff830f036d3604779903be0a7a814e600d1
Bill Stoltz <bill.stoltz@boosterwebsolutions.com>
2018-10-13 18:25:05 -0400

    fix mistake

--- create.js
+++ create.js
@@ -14,8 +14,7 @@ export async function main(event, context, callback) {
       createdAt: Date.now()
     }
   };
-  gibberish.what;
-  
+
   try {
     await dynamoDbLib.call("put", params);
     callback(null, success(params.Item));
